f877a6023fb030b78eb03562353e7c4c943771bd
DlopezS98 <01dlopezs98@gmail.com>
2025-10-01 10:41:21 -0600

    Setting up initial application routes

---
+++ lib/ui/core/routes/app_router.dart
@@ -0,0 +1,14 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:boardforge_app/ui/authentication/screens/login_screen.dart';
+import 'package:boardforge_app/ui/authentication/screens/signup_screen.dart';
+
+import './app_router.gr.dart';
+
+@AutoRouterConfig(replaceInRouteName: "Screen|View,Route")
+class AppRouter extends RootStackRouter {
+  @override
+  List<AutoRoute> get routes => [
+    AutoRoute(page: LoginRoute.page, path: LoginScreen.routeName),
+    AutoRoute(page: SignupRoute.page, path: SignupScreen.routeName),
+  ];
+}
