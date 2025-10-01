c5215741120773cbf75354304f55f76d771330c6
DlopezS98 <01dlopezs98@gmail.com>
2025-10-01 11:14:08 -0600

    Refactor LoginScreen to enhance layout with additional UI elements and navigation options

--- lib/ui/authentication/screens/login_screen.dart
+++ lib/ui/authentication/screens/login_screen.dart
@@ -1,4 +1,5 @@
 import 'package:auto_route/auto_route.dart';
+import 'package:boardforge_app/ui/core/routes/app_router.gr.dart';
 import 'package:flutter/material.dart';
 
 @RoutePage()
@@ -9,6 +10,37 @@ class LoginScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(body: Center(child: Text('Login Screen')));
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Login Screen'),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                AutoRouter.of(context).push(const MainLayoutRoute());
+              },
+              child: Text('Login'),
+            ),
+            SizedBox(height: 20),
+            SizedBox(
+              height: 40,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text("Don't have an account?", style: TextStyle(fontSize: 12, color: Colors.deepPurple)),
+                  const SizedBox(width: 5),
+                  InkWell(
+                    onTap: () => AutoRouter.of(context).push(SignupRoute()),
+                    child: const Text('Sign up', style: TextStyle(color: Colors.blueAccent, fontSize: 12)),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
