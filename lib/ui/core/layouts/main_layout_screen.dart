29294ff650815562e8f2666042ca0d02fabde8a5
DlopezS98 <01dlopezs98@gmail.com>
2025-10-01 11:07:35 -0600

    Refactor MainLayoutScreen to include a Drawer with navigation options and user avatar

--- lib/ui/core/layouts/main_layout_screen.dart
+++ lib/ui/core/layouts/main_layout_screen.dart
@@ -8,6 +8,31 @@ class MainLayoutScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: Text('Main Layout')), body: AutoRouter());
+    return Scaffold(
+      appBar: AppBar(title: Text('Main Layout')),
+      body: AutoRouter(),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  CircleAvatar(
+                    radius: 40,
+                    backgroundImage: NetworkImage('https://avatars.githubusercontent.com/u/48362994?v=4'),
+                  ),
+                  SizedBox(height: 10),
+                  Text('Danny Lopez', style: TextStyle(color: Colors.white, fontSize: 20)),
+                ],
+              ),
+            ),
+            ListTile(title: Text('Teams'), onTap: () => {}),
+            ListTile(title: Text('Projects'), onTap: () => {}),
+          ],
+        ),
+      ),
+    );
   }
 }
