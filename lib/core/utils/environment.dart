57191247c8b6b6f729a4270649e5d65b0bf4e8bd
DlopezS98 <01dlopezs98@gmail.com>
2025-10-15 17:23:14 -0600

    Add environment configuration and integrate dotenv for API management; update Podfile.lock and project settings for Pods

--- lib/core/utils/environment.dart
+++ lib/core/utils/environment.dart
@@ -1,6 +1,14 @@
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+
 /// A singleton class to manage environment variable configurations
 class Environment {
   Environment._(); // Private constructor to prevent instantiation
   static final Environment _instance = Environment._();
   static Environment get instance => _instance;
+
+  String get environment => String.fromEnvironment('ENVIRONMENT', defaultValue: 'development');
+
+  String get baseApiUrl => dotenv.env['BASE_API_URL'] ?? '';
+  String get otherEnvironmentVariable => dotenv.env['OTHER_ENVIRONMENT_VARIABLE'] ?? '';
+  String get pruebaVariable => dotenv.env['PRUEBA_VARIABLE'] ?? '';
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,16 @@
+// import 'package:boardforge_app/core/utils/environment.dart';
+import 'package:boardforge_app/core/utils/environment.dart';
 import 'package:boardforge_app/ui/core/routes/app_router.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() {
+void main() async {
+  // debugPrint('Current Environment: ${Environment.instance.environment}');
+  await dotenv.load(fileName: 'production.env');
+
+  debugPrint('BASE_API_URL: ${Environment.instance.baseApiUrl}');
+  debugPrint('OTHER_ENVIRONMENT_VARIABLE: ${Environment.instance.otherEnvironmentVariable}');
+  debugPrint('PRUEBA_VARIABLE: ${Environment.instance.pruebaVariable}');
   runApp(MainApp());
 }
 
